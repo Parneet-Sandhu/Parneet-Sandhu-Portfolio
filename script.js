ff39701551616dbd312692b9855e961f65ab7351
Parneet Kaur Sandhu <121374723+Parneet-Sandhu@users.noreply.github.com>
2025-10-27 14:12:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-document.addEventListener('contextmenu', (e) => e.preventDefault());
+/*document.addEventListener('contextmenu', (e) => e.preventDefault());
 document.addEventListener('keydown', (e) => {
     if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
         e.preventDefault();
     }
 });
+
+*/
